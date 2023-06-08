f8aa5148c3ea201083afc42efa5c44f619816451
Brandt Kurowski <brandt@kurowski.net>
2023-06-07 21:56:53 -0400

    fix install harder

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-DOTFILES=`dirname $0`
+DOTFILES=`realpath $0`
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc
-ln -s .dotfiles/.zshrc ~
+ln -s $DOTFILES/.zshrc ~
 rm ~/.p10k.zsh
-ln -s .dotfiles/.p10k.zsh ~
+ln -s $DOTFILES/.p10k.zsh ~
