1b64480b8d4415b10424891817eedb2a55f2c2d3
Brandt Kurowski <brandt@kurowski.net>
2023-06-08 11:01:46 -0400

    preserve original dotfiles

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 DOTFILES=$(dirname $(realpath $0))
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-rm ~/.zshrc
+[[ ! -f ~/.zshrc ]] || mv ~/.zshrc ~/.zshrc.orig
 ln -s $DOTFILES/.zshrc ~
-rm ~/.p10k.zsh
+[[ ! -f ~/.p10k.zsh ]] || mv ~/.p10k.zsh ~/.p10k.zsh.orig
 ln -s $DOTFILES/.p10k.zsh ~
