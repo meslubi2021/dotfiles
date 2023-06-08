829226178e5079909439649e65f5cef4c023edb9
Brandt Kurowski <brandt@kurowski.net>
2023-06-07 22:06:06 -0400

    maybe fix highlighting

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 DOTFILES=$(dirname $(realpath $0))
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc.local
 rm ~/.zshrc
 ln -s $DOTFILES/.zshrc ~
 rm ~/.p10k.zsh
