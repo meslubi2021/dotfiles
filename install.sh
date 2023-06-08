374f6b86726b4f40e3bf7749f2ad131a92e36b04
Brandt Kurowski <brandt@kurowski.net>
2023-06-08 17:37:54 -0400

    fix zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 DOTFILES=$(dirname $(realpath $0))
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
