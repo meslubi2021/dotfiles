142479533ac4c9a77ca4fe8c6b152ab93f3f6b75
Brandt Kurowski <brandt@kurowski.net>
2023-06-07 21:52:50 -0400

    fix install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+DOTFILES=`dirname $0`
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm ~/.zshrc
 ln -s .dotfiles/.zshrc ~
 rm ~/.p10k.zsh
